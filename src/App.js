6cd04c810afd12ec7e7e1f7a6e5f577a65c37f38
urmiljoshi <urmiljoshi16@gmail.com>
2022-07-12 10:41:58 +0530

    12_7_merging

--- src/App.js
+++ src/App.js
@@ -2,45 +2,60 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  //================== is Array ======================//
-  // let arr =[21,22,23,24,25];
-  //  let ans= Array . isArray(arr)
-  //  console.log(ans);   //true
+   //====== Array =====//
 
-//==================== concat =========================//
-  // let arr = [21,22,23,24,25];
-   //let arr1 =[199,200];
-  // let newarr = arr.concat(arr1);
-  // console.log(newarr); //) [21, 22, 23, 24, 25, 199, 200]
+//  let arr =[5,4,6,8,9];
 
-//=========================================================//
-  //let arr = [21,22,23,24,25];
-  //let arr1 =[199,200];
-  // let ans = arr.some((a) => a =>30);
-  // console.log(ans);  // true
+//=================== copy array =====================//
+
+//let arr1=[...arr];
+//console.log(arr1); // 5,4,6,8,9
+
+//========================== merging array ======================//
+
+// let arr =[1,2,3,54,5];
+// let arr2=[100,200];
+
+// let ans = [...arr,...arr2];
+// console.log(ans); //(7) [1, 2, 3, 54, 5, 100, 200]
+
+//===================== destructuring =========================//
+
+// let arr = [11,12,15,14,18];
+// const [j,h,g,f,r] = arr;
+// console.log(g);  //15
+
+//========== Object =========//
+
+//  let obj ={
+//    id: 111,
+//    name: 'ajay'
+// }
+
+//=================== copy Object =====================//
+//let obj1={...obj};
+//console.log(obj1); //{id: 111, name: 'ajay'}
+
+//========================== merging obj======================//
+
+//let obj2={
+  //id:6462,
+  // name:'urmil',
+  //place:'surat'
+// }
+// let ans={...obj,...obj2};
+// console.log(ans); //{id: 6462, name: 'urmil', place: 'surat'}
+//===================== destructuring =====================//
  
-  //======================== to string ===================================//
+// let obj={
+//   id:6462,
+//   name:'urmil'
+// }
 
-  // let arr =[21,22,23,24,25];
-  // console.log(arr.toString(arr));//21,22,23,24,25
-//=================== find ==========================//  
-  // let arr =[21,22,23,24,25];
-  //  let ans =arr.find ((a) => a =>30);
-  //  console.log(ans);  //21,22,23,24,25
+// let {id,name}=obj
+// console.log(name,obj.id); //urmil 6462
 
-  //let arr =[21,22,23,24,25];
-  // arr.splice(2,0,99); //  [21, 22, 99, 23, 24, 25] element add 
- // arr.splice(2,2)  // element delet .
-  //console.log(arr); 
-
-  //=================== object ========================//
-  //    let obj = {
-  //   id : 6462,
-  //   namr : "urmil"
-  // };
-  // console.log(obj.id,obj["name"]); //6462 {id: 6462, namr: 'urmil'}
-
-  return (
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
