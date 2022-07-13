626675786ec99332c5981178f0bbe2ff94f28b1f
urmiljoshi <urmiljoshi16@gmail.com>
2022-07-13 11:37:16 +0530

    13_7_function

--- src/App.js
+++ src/App.js
@@ -5,60 +5,56 @@ function App() {
    let data=
     [
       {
-        id: 101,
-        name: 'Abacavir',
-        quantity: 25,
-        price: 150,
-        expiry: 2022,
+        name: "amit",
+        age: 35,
+        salary: 40000,
+        bonus: 1000,
         status: true
       },
       {
-        id: 102,
-        name: 'Eltrombopag',
-        quantity: 90,
-        price: 550,
-        expiry: 2021,
-        status: true
-      },
-      {
-        id: 103,
-        name: 'Meloxicam',
-        quantity: 85,
-        price: 450,
-        expiry: 2025,
+        name: "ajay",
+        age: 25,
+        salary: 38000,
+        bonus: 2000,
         status: false
       },
       {
-        id: 104,
-        name: 'Allopurinol',
-        quantity: 50,
-        price: 600,
-        expiry: 2023,
+        name: "mayur",
+        age: 23,
+        salary: 50000,
+        bonus: 500,
         status: true
       },
       {
-        id: 105,
-        name: 'Phenytoin',
-        quantity: 63,
-        price: 250,
-        expiry: 2021,
-        status: false
-      }
-     ];
+        name: "jay",
+        age: 29,
+        salary: 35000,
+        bonus: 600,
+        status: true
+      },
+      {
+        name: "raj",
+        age: 33,
+        salary: 22000,
+        bonus: 2000,
+        status: true
+      },
+     ]
 
-    let fdate = data.filter((v,i) => v.expiry >=2022 && v.status===true);
-     console.log(fdate);
+    let mdata = data.filter((v,i) => v.status === true);
+     console.log(mdata);
 
-     let totalprice = fdate.reduce((acc,v,i) => acc + v.price,0)
-     console.log(totalprice);
+     let salary = mdata.reduce((acc,v,i) => acc + v.salary + v.bonus ,0)
+     console.log(salary); 
 return (
    <div>
         {
-         fdate.map((v,i) =>{
+       data.map((v,i) =>{
             return (
               <div key = {i}>
                 <h1>{v.name}</h1>
-                <h4>{v.price}</h4>
+                <h4>{v.bonus}</h4>
+                <h5>{v. salary}</h5>
               </div>
             )
           })
